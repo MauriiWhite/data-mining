7619b4db0db29aed1c12dbb95a0f40bb6db1c31a
MauriiWhite <mauriblanco.s@gmail.com>
2024-09-30 21:56:41 -0300

    feat: statistic file created with metrics

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
-from metrics.statistics import metrics
+from metrics.statistics import time_metrics
+from pprint import pprint
 
 FILE = "data/films.csv"
 
-def main():
-    df = pd.read_csv(FILE)
-    df = df.dropna()
-    metrics(df)
-    
-    
+
+def main() -> None:
+    df = pd.read_csv(FILE).dropna()
+
+    df["fecha"] = pd.to_datetime(df["fecha"])
+    df["precio"] = pd.to_numeric(df["precio"], errors="coerce")
+    df["cantidad"] = pd.to_numeric(df["cantidad"], errors="coerce")
+    df["sells"] = df["precio"] * df["cantidad"]
+
+    pprint(time_metrics(df), sort_dicts=False)
+
+
 if __name__ == "__main__":
     main()
--- src/metrics/statistics.py
+++ src/metrics/statistics.py
@@ -2,18 +2,102 @@ import pandas as pd
 
 from pandas.core.frame import DataFrame
 
-def metrics(df: DataFrame) -> None:
-    df['precio'] = pd.to_numeric(df['precio'], errors='coerce')
-    df['cantidad'] = pd.to_numeric(df['cantidad'], errors='coerce')
-    df['ventas'] = df['precio'] * df['cantidad']
-    total_ventas = df['ventas'].sum()
-    numero_transacciones = len(df)
+def metrics(df: DataFrame) -> dict:
+    sells = df['sells'].sum()
+    transactions = len(df)
+    movies = df['pelicula'].nunique()
+    payments = len(df['metodo_pago'].unique())
+    date_range = (df['fecha'].max() - df['fecha'].min()).days
     
-    print(f"Total de ventas: {total_ventas}")
-    print(f"Número de transacciones: {numero_transacciones}")
-    print(f"Promedio de ventas por transacción: {total_ventas / numero_transacciones}")
-    print(f"Máximo de ventas por transacción: {df['ventas'].max()}")
-    print(f"Mínimo de ventas por transacción: {df['ventas'].min()}")
+    
+    return {
+        'sells': sells,
+        'transactions': transactions,
+        'average_sells': round(sells / transactions, 2),
+        'max_sells': df['sells'].max(),
+        'min_sells': df['sells'].min(),
+        'movies': movies,
+        'date_range': date_range,
+        'payments': payments,
+        'start_date': df['fecha'].min().date(),
+        'end_date': df['fecha'].max().date(),
+    }
+    
 
-if __name__ == "__main__":
-    metrics()
+def film_metrics(df: DataFrame) -> dict:
+    films = df.groupby('pelicula')
+    
+    sells_by_film = round(films['sells'].sum(), 1).to_dict()
+    media_price_by_film = round(films['precio'].mean(), 1).to_dict()
+    amount_by_film = round(films['pelicula'].count(), 1).to_dict()
+    film_max_sells = films['cantidad'].sum().idxmax()
+    film_min_sells = films['sells'].sum().idxmin()
+    
+    return {
+        'sells_by_film': sells_by_film,
+        'media_price_by_film': media_price_by_film,
+        'amount_by_film': amount_by_film,
+        'film_max_sells': film_max_sells,
+        'film_min_sells': film_min_sells
+    }
+
+def gender_metrics(df: DataFrame) -> dict:
+    gender = df.groupby('genero')
+    
+    sells_by_gender = round(gender['sells'].sum(), 1).to_dict()
+    media_price_by_gender = round(gender['precio'].mean(), 1).to_dict()
+    amount_by_gender = round(gender['genero'].count(), 1).to_dict()
+    gender_max_sells = gender['sells'].sum().idxmax()
+    gender_min_sells = gender['sells'].sum().idxmin()
+    
+    return {
+        'sells_by_gender': sells_by_gender,
+        'media_price_by_gender': media_price_by_gender,
+        'amount_by_gender': amount_by_gender,
+        'gender_max_sells': gender_max_sells,
+        'gender_min_sells': gender_min_sells
+    }
+    
+
+def payment_metrics(df: DataFrame) -> dict:
+    payment = df.groupby('metodo_pago')
+    
+    sells_by_payment = round(payment['sells'].sum(), 1).to_dict()
+    media_price_by_payment = round(payment['precio'].mean(), 1).to_dict()
+    amount_by_payment = round(payment['metodo_pago'].count(), 1).to_dict()
+    payment_max_sells = payment['sells'].sum().idxmax()
+    payment_min_sells = payment['sells'].sum().idxmin()
+    
+    # 15. Número de Transacciones con Error (asumiendo que los errores se marcan como NaN)
+    number_of_errors = df[df['sells'].isna()].shape[0]
+    
+    proportion_of_errors = round(number_of_errors / len(df) * 100, 1) if number_of_errors > 0 else 0
+    
+    
+    return {
+        'sells_by_payment': sells_by_payment,
+        'media_price_by_payment': media_price_by_payment,
+        'amount_by_payment': amount_by_payment,
+        'payment_max_sells': payment_max_sells,
+        'payment_min_sells': payment_min_sells,
+        'number_of_errors': number_of_errors,
+        'proportion_of_errors': proportion_of_errors
+    }
+    
+def time_metrics(df: DataFrame) -> dict:
+    df['fecha'] = pd.to_datetime(df['fecha'])
+    date = df.groupby(df['fecha'].dt.to_period('M'))
+    
+    sells_by_month = round(date['sells'].sum(), 1).to_dict()
+    media_price_by_month = round(date['precio'].mean(), 1).to_dict()
+    amount_by_month = round(date['fecha'].count(), 1).to_dict()
+    month_max_sells = date['sells'].sum().idxmax()
+    month_min_sells = date['sells'].sum().idxmin()
+    
+    return {
+        'sells_by_month': sells_by_month,
+        'media_price_by_month': media_price_by_month,
+        'amount_by_month': amount_by_month,
+        'month_max_sells': month_max_sells,
+        'month_min_sells': month_min_sells
+    }
